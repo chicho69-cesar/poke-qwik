0dfdc95cee4dd455109fbcb811727add4710824a
chichodev <122078038+chichodev@users.noreply.github.com>
2023-06-14 14:43:28 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,11 +10,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Poke Qwik',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'Aplicación de Pokedex para aprender Qwik!',
     },
   ],
 };
